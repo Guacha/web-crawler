75648a8642c5e854f8f7b0b7f1f40e34f5dff3e8
Guacha <Guacha@Guacha-PC>
2019-08-31 11:31:59 -0500

    Comentarios, JavaDocs y otras cosas añadidas, funciones innecesarias corregidas

--- src/main/java/com/labr4/crawler/Webber.java
+++ src/main/java/com/labr4/crawler/Webber.java
@@ -11,7 +11,6 @@ import java.util.List;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 /**
